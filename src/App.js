c5bea9a8f70d2474b264eede690fcd91a3e474d6
ciutek <stencelkamil15@gmail.com>
2024-10-01 10:57:17 +0200

    komit konrad wez mnie stont

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +18,23 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">kakaraka</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    <div id="emailHelp" class="form-text">nie chcesz</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">haslo</label>
+    <input type="password" class="form-control" id="exampleInputPassword1"/>
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
+    <label class="form-check-label" for="exampleCheck1">get lost</label>
+  </div>
+  <button type="submit" class="btn btn-primary">susuususu   </button>
+</form>
     </div>
   );
 }
